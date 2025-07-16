9e8dd28abbd244b363e8d355d6c52a2646c549ca
Haru <tgv5612@gmail.com>
2025-07-16 16:51:30 +0900

    2025-07-16 14장까지 완료

---
+++ src/main/java/com/example/firstproject/entity/Comment.java
@@ -0,0 +1,24 @@
+package com.example.firstproject.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+@Getter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    @JoinColumn(name="article_id")
+    private Article article;
+    @Column
+    private String nickname;
+    @Column
+    private String body;
+}
