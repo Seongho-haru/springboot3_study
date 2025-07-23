a889b1d1521ab3c8f3fbda6cc7c61cc391e22023
Haru <tgv5612@gmail.com>
2025-07-23 16:50:31 +0900

    2025-07-23 15.4장까지 완료

--- src/main/java/com/example/firstproject/entity/Comment.java
+++ src/main/java/com/example/firstproject/entity/Comment.java
@@ -1,5 +1,6 @@
 package com.example.firstproject.entity;
 
+import com.example.firstproject.dto.CommentDto;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -21,4 +22,18 @@ public class Comment {
     private String nickname;
     @Column
     private String body;
+
+    public static Comment createComment(CommentDto dto, Article article) {
+        if (dto.getId() != null){
+            throw new IllegalArgumentException("댓글 생성 실패! 댓글의 id가없어야 합니다!");
+        }
+        if (dto.getArticleId() != article.getId())
+            throw new IllegalArgumentException("댓글 생성 실패! 게시그의 id가 잘못되었습니다");
+        return new Comment(
+                dto.getId(),
+                article,
+                dto.getNickname(),
+                dto.getBody()
+        );
+    }
 }
